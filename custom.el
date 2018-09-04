54bfc8cc872cea46fb61623b8f4d2d6ef2c3fdbb
Dyachenko Ivan <vandyachen@gmail.com>
2018-09-04 13:42:28 +0300

    custom.el

--- custom.el
+++ custom.el
@@ -3,12 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" default)))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" default)))
  '(package-selected-packages
    (quote
-    (grayscale-theme emacs-grayscale-theme irony company-irony base16-default-dark base16-theme csv-mode smart-tabs-mode highlight-parentheses highlight-quoted highlight-defined rainbow-mode rainbow-delimiters no-littering nlinum linum-off all-the-icons-dired dired+ neotree all-the-icons elpy json-mode centered-window-mode gruvbox-theme gruvbox-dark-hard darktooth-theme sublime-themes em-hist em-term em-prompt em-sma esh-opt magit popup-imenu undo-tree projectile scala-mode ensime exec-path-from-shell markdown-mode helm workgroups2 use-package dashboard ace-window ace-jump-mode))))
+    (aggressive-indent clean-aindent-mode cmake-font-lock cmake-mode clang-format google-c-style color-theme-sanityinc-tomorrow grayscale-theme emacs-grayscale-theme irony company-irony base16-default-dark base16-theme csv-mode smart-tabs-mode highlight-parentheses highlight-quoted highlight-defined rainbow-mode rainbow-delimiters no-littering nlinum linum-off all-the-icons-dired dired+ neotree all-the-icons elpy json-mode centered-window-mode gruvbox-theme gruvbox-dark-hard darktooth-theme sublime-themes em-hist em-term em-prompt em-sma esh-opt magit popup-imenu undo-tree projectile scala-mode ensime exec-path-from-shell markdown-mode helm workgroups2 use-package dashboard ace-window ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
