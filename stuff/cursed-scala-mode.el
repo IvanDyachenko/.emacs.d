be5ccb4c85c9e5641c7cdf0f16b5ec2e700e4211
Ivan Dyachenko <vandyachen@gmail.com>
2019-05-27 18:26:27 +0300

    cursed Scala mode: use metals instead of ensime

--- stuff/cursed-scala-mode.el
+++ stuff/cursed-scala-mode.el
@@ -24,28 +24,35 @@
 
 ;;; Code:
 
-(use-package ensime
-  :pin melpa-stable
-  :init (setq ensime-startup-notification nil))
+;; Enable scala-mode and sbt-mode
+(use-package scala-mode
+  :mode "\\.s\\(cala\\|bt\\)$")
 
 (use-package sbt-mode
-  :commands (sbt-start sbt-command)
-  :hook (sbt-mode . (lambda ()
-                      (progn
-                        (setq-local comint-use-prompt-regexp nil)
-                        (setq-local inhibit-field-text-motion nil))))
+  :commands sbt-start sbt-command
   :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
   (substitute-key-definition
    'minibuffer-complete-word
    'self-insert-command
    minibuffer-local-completion-map))
 
-;;
-(setq-local comint-use-prompt-regexp nil)
-(setq-local inhibit-field-text-motion nil)
+;; Enable nice rendering of diagnostics like compile errors.
+(use-package flycheck
+  :init (global-flycheck-mode))
 
-(use-package scala-mode
-  :interpreter ("scala" . scala-mode))
+(use-package lsp-mode
+  :init (setq lsp-prefer-flymake nil))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+(use-package lsp-scala
+  :after scala-mode
+  :demand t
+  ;; Optional - enable lsp-scala automatically in scala files
+  :hook (scala-mode . lsp))
 
 (provide 'cursed-scala-mode)
 ;;; cursed-scala-mode.el ends here
