6693aca4b995f4be767cab629622265caf994b4b
Ivan <vandyachen@gmail.com>
2020-03-21 23:32:14 +0300

    Update cursed scala mode

--- stuff/cursed-scala-mode.el
+++ stuff/cursed-scala-mode.el
@@ -36,28 +36,47 @@
   (substitute-key-definition
    'minibuffer-complete-word
    'self-insert-command
-   minibuffer-local-completion-map))
+   minibuffer-local-completion-map)
+  ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false")))
 
-;; Enable nice rendering of diagnostics like compile errors.
+;; Enable nice rendering of diagnostics like compile errors
 (use-package flycheck
   :init (global-flycheck-mode))
 
 (use-package lsp-mode
-  :init
-  (setq lsp-prefer-flymake nil)
-  (setq lsp-enable-snippet nil)
-  :hook (scala-mode . lsp))
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook
+  (scala-mode . lsp)
+  (lsp-mode . lsp-lens-mode)
+  :config (setq lsp-prefer-flymake nil))
 
+;; Enable nice rendering of documentation on hover
 (use-package lsp-ui
   :hook (lsp-mode . lsp-ui-mode))
 
+;; lsp-mode supports snippets, but in order for them to work you need to use yasnippet
+;; If you don't want to use snippets set lsp-enable-snippet to nil in your lsp-mode settings
+;;   to avoid odd behavior with snippets and indentation
+(use-package yasnippet)
+
 (use-package company-lsp)
 
-;;(use-package lsp-scala
-;;  :after scala-mode
-;;  :demand t
-;;  ;; Optional - enable lsp-scala automatically in scala files
-;;  :hook (scala-mode . lsp))
+;; Use the Debug Adapter Protocol for running tests and debugging
+(use-package posframe
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+  )
+
+(use-package dap-mode
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode))
+
+;; Use the Tree View Protocol for viewing the project structure and triggering compilation
+(use-package lsp-treemacs
+  :config
+  (lsp-metals-treeview-enable t)
+  (setq lsp-metals-treeview-show-when-views-received t))
 
 (provide 'cursed-scala-mode)
 ;;; cursed-scala-mode.el ends here
