23edce5af86f68ecb629fd9da86935b2c95566e5
Ivan Dyachenko <vandyachen@gmail.com>
2022-09-19 12:33:07 +0300

    Remove `wakatime-cli-path` and `wakatime-python-bin` from `custom.el`

--- custom.el
+++ custom.el
@@ -5,9 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    ()))
- '(wakatime-cli-path "/usr/local/bin/wakatime")
- '(wakatime-python-bin nil))
+    ())))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
